651cfe546c8ef2ca0430a88b742fbbb5efcbc27a
Josue Zarzosa <josue.zarzosa@esailors.de>
2021-12-13 17:57:07 +0000

    lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { LibsStoreUiSharedModule } from '@bg-hoard/libs/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,8 +12,10 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
     MatCardModule,
+    LibsStoreUiSharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
