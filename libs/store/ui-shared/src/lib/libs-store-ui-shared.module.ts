5d84d4232b5227aa647810d6633f3bc6e56bea20
Josue Zarzosa <josue.zarzosa@esailors.de>
2021-12-14 15:15:22 +0000

    Lab 13

--- libs/store/ui-shared/src/lib/libs-store-ui-shared.module.ts
+++ libs/store/ui-shared/src/lib/libs-store-ui-shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FeatureGameDetailModule } from '@bg-hoard/feature-game-detail';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, FeatureGameDetailModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
